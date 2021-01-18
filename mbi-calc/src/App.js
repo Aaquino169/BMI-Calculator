c22285752a472d0b1abe3a235ffb92e0121c081c
alexander_aquino <nanito169@gmail.com>
2021-01-18 12:06:25 -0500

    created react app and edited app component

---
+++ mbi-calc/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BMI Calculator</h1>
+      <h2>Your BMI and Healthy Weight Range</h2>
+      <p>There's no "perfect weight" that fits everyone. BMI, or body mass index, measures how healthy your weight is based on how tall you are. It gives you a clue to your risk for weight-related health problems.</p>
+      <p>If you're over 20 and not pregnant, find out what your number is and what it means.</p>
+      
+    </div>
+  );
+}
+
+export default App;
